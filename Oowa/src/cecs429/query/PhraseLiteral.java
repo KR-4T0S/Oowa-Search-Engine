490779cc70ac6be0c899f8dd4e90fb860a6c0ddf
Richie E <repinosa@gmail.com>
2019-10-08 18:34:04 -0700

    fix bug in phrase query with repeated words

--- Oowa/src/cecs429/query/PhraseLiteral.java
+++ Oowa/src/cecs429/query/PhraseLiteral.java
@@ -3,6 +3,7 @@ package cecs429.query;
 import cecs429.index.Index;
 import cecs429.index.Posting;
 import cecs429.text.*;
+import static java.lang.Math.abs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -52,59 +53,121 @@ public class PhraseLiteral implements QueryComponent {
                 for (String s : processor.processToken(mTerms.get(i))) {
                     tempComponentResults = unionMergePostings(tempComponentResults, index.getPostings(s));
                 }
+
+                result = positionalIntersect(result, tempComponentResults, i);
+            }
+        }
+
+        return result;
+    }
+
+     private List<Posting> forwardPositionalIntersect(List<Posting> p1, List<Posting> p2, int k) {
+        List<Posting> result = new ArrayList();
+        
+        int i = 0, j = 0;
+        while (i < p1.size() && j < p2.size()) {                           
+            if (p1.get(i).getDocumentId() == p2.get(j).getDocumentId()) {
+                List<Integer> l = new ArrayList();
+                Posting posting = new Posting(p1.get(i).getDocumentId());
+                List<Integer> pp1 = p1.get(i).getPositions();
+                List<Integer> pp2 = p2.get(j).getPositions();
                 
-                // Returns positions where WORD matches
-                result = positionalIntersectMergePostings(result, tempComponentResults);
-            }
-        }
+                int m = 0, n = 0;
+                while (m < pp1.size() && n < pp2.size()) {
+                    if (pp2.get(n) > pp1.get(m)) {
+                        if (pp2.get(n) - pp1.get(m) == k) {
+                            System.out.println("\tIDs: " + p1.get(i).getDocumentId() + " | " + p2.get(j).getDocumentId());
+                            System.out.println("\t\tMatch: " + pp1.get(m) + " | " + pp2.get(n));
 
-        return result;
-    }
+                            l.add(pp1.get(m));
 
-    
-    
-    private List<Posting> positionalIntersectMergePostings(List<Posting> listA, List<Posting> listB) {
-        List<Posting> result = new ArrayList(); // Placeholder List
-
-        if (!listA.isEmpty() || !listB.isEmpty()) { // Neither list can be empty
-            int i = 0, j = 0;
-            while (i < listA.size() && j < listB.size()) {
-                if (listA.get(i).getDocumentId() < listB.get(j).getDocumentId()) {
-                    i++;
-                } else if (listA.get(i).getDocumentId() > listB.get(j).getDocumentId()) {
-                    j++;
-                } else {
-                    // Same doc 
-                    // Positional test
-                    List<Integer> positionsA = listA.get(i).getPositions();
-                    List<Integer> positionsB = listB.get(j).getPositions();
-
-                    int k = 0; // Pos A
-                    int l = 0; // Pos B
-                    // [to, be, or, not, to, be]
-                    while ((k < positionsA.size() && l < positionsB.size())) {
-                        if ((positionsA.get(k) <= positionsB.get(l))) { // could be same word
-                            // A behind B
-                            if (positionsB.get(l) - positionsA.get(k) == 1) {
-                                result.add(listB.get(j));
-                                break;
-                            } else {
-                                k++;
-                            }
-                        } else { // Pos of first word is equal to 
-                            l++;
+                            n++;
+                            m++;
+                        } else {
+                            n++;
                         }
+                    } else if (pp2.get(n) < pp1.get(m)) {
+                        m++;
+                    } else { // both pos equal
+                        n++;
+                        m++;
                     }
-                    
-                    // Did not match, Next Doc
-                    i++;
-                    j++;
+                }     
+                if (!l.isEmpty()) {
+                    for (Integer ps: l) {
+                        posting.addPos(ps);
+                    }
+                    result.add(posting);
                 }
+                
+                i++;
+                j++;
+            } else if (p1.get(i).getDocumentId() < p2.get(j).getDocumentId()) {
+                i++;
+            } else {
+                j++;
             }
         }
-
+        
+        System.out.print("RESULT: \n\t[");
+        if (!result.isEmpty()) {
+            for (Posting p: result) {
+                System.out.print(p.getDocumentId() + " | ");
+            }
+        }
+        System.out.print("]\n");
+        
+        
         return result;
     }
+    
+    private List<Posting> positionalIntersect(List<Posting> p1, List<Posting> p2, int k) {
+        List<Posting> result = new ArrayList();
+        
+        int i = 0, j = 0;
+        while (i < p1.size() && j < p2.size()) {                           
+            if (p1.get(i).getDocumentId() == p2.get(j).getDocumentId()) {
+                List<Integer> l = new ArrayList();
+                List<Integer> pp1 = p1.get(i).getPositions();
+                List<Integer> pp2 = p2.get(j).getPositions();
+                
+                int m = 0, n = 0;
+                while (m < pp1.size()) {
+                    while (n < pp2.size()) {
+                        //System.out.println("\tComparing: " + pp1.get(m) + " | " + pp2.get(n));
+                        if (pp2.get(n) - pp1.get(m) == k) {
+                            //System.out.println("\t\tMatch: " + pp1.get(m) + " | " + pp2.get(n));
+                            l.add(pp1.get(m));
+                        } 
+                        else if (pp2.get(n) > pp1.get(m)) {
+                            //System.out.println("\t\tNO Match: " + pp1.get(m) + " | " + pp2.get(n));
+                            break;
+                        }
+                        n++;
+                    }
+                    m++;
+                }
+                
+                if (!l.isEmpty()) {
+                    Posting tempPosting = new Posting(p1.get(i).getDocumentId());
+                    for (Integer ps: l) {
+                        tempPosting.addPos(ps);
+                    }
+                    result.add(tempPosting);
+                }
+                
+                i++;
+                j++;
+            } else if (p1.get(i).getDocumentId() < p2.get(j).getDocumentId()) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        
+        return result;
+    }
+    
 
     private List<Posting> unionMergePostings(List<Posting> listA, List<Posting> listB) {
         List<Posting> result = new ArrayList(); // Placeholder List
